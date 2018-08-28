c24bc7f62d2bdfa609b0aba152b82f539736c78f
cymruhangu <pragquix@gmail.com>
2018-08-27 22:27:12 -0400

    changed to mlab DB

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 "use strict";
 exports.DATABASE_URL =
-  process.env.DATABASE_URL || "mongodb://localhost/restaurants-app";
+  process.env.DATABASE_URL || "mongodb://admin2:D0nt4g3t!@ds127342.mlab.com:27342/restaurant-app";
 exports.TEST_DATABASE_URL =
-  process.env.TEST_DATABASE_URL || "mongodb://localhost/test-restaurants-app";
+  process.env.TEST_DATABASE_URL || "mongodb://admin2:D0nt4g3t!@ds127342.mlab.com:27342/test-estaurant-app";
 exports.PORT = process.env.PORT || 8080;
